41eece5448925eaf6c230581755ce3bc90964513
tercoz <tercoz@mail.ru>
2021-04-08 12:17:02 +0300

    Lk is polished a little bit

--- src/main/java/ru/fber/cabinet/CabinetApplication.java
+++ src/main/java/ru/fber/cabinet/CabinetApplication.java
@@ -2,19 +2,38 @@ package ru.fber.cabinet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
 @RestController
-public class CabinetApplication {
+public class CabinetApplication extends WebSecurityConfigurerAdapter {
 
     @GetMapping("/")
-    public Map<String, Object> root() {
-        return Collections.singletonMap("login", "whatever");
+    public Map<String, Object> root(@AuthenticationPrincipal OAuth2User oauth2User, HttpSession session) {
+        Map<String, Object> map = new HashMap<>(oauth2User.getAttributes());
+        map.put("slavs", session.getAttribute("slavs"));
+        return map;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize.anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                );
     }
 
     public static void main(String[] args) {
